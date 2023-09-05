471baf892ea8f231eb57a808c052cdf9114bd8d8
Samiylo <skryshu@gmail.com>
2023-09-05 17:59:50 -0500

    Init

---
+++ src/test/java/com/hft/hftsystem/HftsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hft.hftsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HftsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
